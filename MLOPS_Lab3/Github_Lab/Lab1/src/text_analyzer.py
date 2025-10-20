efb6706d4cd63ff910ee10c2a0b47e738a5b8232
ProfSchrodinger <sabarimathavan24@gmail.com>
2025-10-20 11:55:53 -0400

    created text_analyzer

---
+++ MLOPS_Lab3/Github_Lab/Lab1/src/text_analyzer.py
@@ -0,0 +1,35 @@
+# src/text_analyzer.py
+
+import re
+
+def count_words(text):
+    """Counts the number of words in a given text."""
+    if not isinstance(text, str):
+        return 0
+    words = text.split()
+    return len(words)
+
+def count_characters(text):
+    """Counts the total number of characters, including spaces."""
+    if not isinstance(text, str):
+        return 0
+    return len(text)
+
+def count_sentences(text):
+    """Counts the number of sentences based on ., !, ? delimiters."""
+    if not isinstance(text, str):
+        return 0
+    # Use regex to find sentence-ending punctuation
+    sentences = re.findall(r'[^.!?]+[.!?]', text)
+    return len(sentences)
+
+def analyze_text(text):
+    """Provides a summary dictionary of text analysis."""
+    if not isinstance(text, str):
+        return {}
+    analysis = {
+        'word_count': count_words(text),
+        'character_count': count_characters(text),
+        'sentence_count': count_sentences(text)
+    }
+    return analysis
